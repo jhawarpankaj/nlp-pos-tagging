51d27ac87db166e46177d84da2db91b7df9f9c90
pankaj <jhawar.pankaj1990@gmail.com>
2019-10-13 23:26:54 -0500

    Added all changes

--- pxj180019_memm.py
+++ pxj180019_memm.py
@@ -1,10 +1,16 @@
 from nltk.corpus import brown
+from scipy.sparse import csr_matrix
+from sklearn.linear_model import LogisticRegression
 import numpy
 import scipy
 import sklearn
 import copy
 import re
-
+tagset = list()
+tag_dict = dict()
+feature_dict = dict()
+rare_words = set()
+obj = LogisticRegression(class_weight='balanced', solver='saga', multi_class='multinomial')
 
 def word_ngram_features(i, words):
     words_copy = copy.copy(words)
@@ -66,105 +72,91 @@ def get_features(i, words, prevtag, rare_words):
     return lower_result
 
 
-def build_Y(tags, tag_dict=dict()):
+def build_Y(tags):
     Y = list()
     for sen_tag in tags:
         for tag in sen_tag:
             index = tag_dict[tag]
             Y.append(index)
-
     return numpy.array(Y)
 
 
+def build_X(features):
+    examples_list, features_list, count = [], [], -1
 
-def main():
-    brown_sentences = brown.tagged_sents(tagset='universal')
-    print(brown_sentences)
-    print(len(brown_sentences))
+    for m in range(len(features)):
+        for n in range(len(features[m])):
+            count = count + 1
+            for i in range(len(features[m][n])):
+                index = feature_dict.get(features[m][n][i], -1)
+                if index != -1:
+                    features_list.append(index)
+                    examples_list.append(count)
+    values = [1] * len(examples_list)
+
+    data = numpy.array(values)
+    row = numpy.array(examples_list)
+    col = numpy.array(features_list)
+    return csr_matrix((data, (row, col)), shape=(count + 1, len(feature_dict)))
 
 
-    # Initializing training sentences and training tags.
+def load_test(filename):
+    result = list(list())
+    with open(filename, "r") as f:
+        for line in f:
+            temp = line.strip().split(" ")
+            result.append(temp)
+    return result
 
-    train_sentences = list(list())
-    train_tags = list(list())
-    words_count = dict()
-    rare_words = set()
-    for sen_tag_list in brown_sentences:
-        temp_word = list()
-        temp_tag = list()
-        for word, tag in sen_tag_list:
-            temp_word.append(word)
-            temp_tag.append(tag)
-            count = words_count.get(word, 0)
-            words_count[word] = count + 1
-        train_sentences.append(temp_word)
-        train_tags.append(temp_tag)
-    for k, v in words_count.items():
-        if v < 5:
-            rare_words.add(k)
-    # print(train_sentences)
-    # print(train_tags)
-    # print(rare_words)
-    word_ngram_features(train_sentences[0].__len__() - 1, train_sentences[0])
-    # print(str(word_features("pankaj", rare_words)))
-    # print(str(word_features("PANKAJ", rare_words)))
-    # print(str(word_features("pank123", rare_words)))
-    # print(str(word_features("pan-", rare_words)))
-    # print(str(word_features("pan", rare_words)))
-    # print(str(get_features(4, train_sentences[0], "AA", rare_words)))
 
-    # Generating features for all training sentences.
+def get_predictions(test_sentence, model):
+    sentence = test_sentence
+    n = len(sentence)
+    T = len(tagset)
+    Y_pred = numpy.empty([n - 1, T, T], dtype=float)
 
-    training_features = list(list())
-    for i in range(len(train_sentences)):
-        temp_sen_features = list()
-        for j in range(len(train_sentences[i])):
-            temp_word_features = list()
-            if j == 0:
-                temp_word_features = temp_word_features + get_features(j, train_sentences[i], "<S>", rare_words)
-            else:
-                temp_word_features = temp_word_features + get_features(j, train_sentences[i], train_tags[i][j - 1], rare_words)
-            temp_sen_features.append(temp_word_features)
-        training_features.append(temp_sen_features)
+    for i in range(1, len(sentence)):
+        for j in range(len(tagset)):
+            test_feature = get_features(i, sentence, tagset[j], rare_words)
+            X = build_X([[test_feature]])
+            Y_pred[i-1][j] = model.predict_log_proba(X)
+    test_feature = get_features(0, sentence, "<S>", rare_words)
+    first_sen = build_X([[test_feature]])
+    Y_start = model.predict_log_proba(first_sen)
+    return Y_pred, Y_start
 
-    print(training_features[0][0][0])
-    print(training_features[0][0][1])
-    print(training_features[0][0][2])
 
-    with open("original_feature.txt", "w+") as f:
-        f.write(str(training_features))
+def viterbi(Y_start, Y_pred):
+    n = len(Y_pred) + 1
+    T = len(tagset)
+    V = numpy.empty([n, T])
+    BP = numpy.empty([n, T], dtype=int)
+    T1 = numpy.empty(len(tagset))
+    for j in range(len(tagset)):
+        V[0, j] = Y_start[0, j]
+        BP[0, j] = -1
 
-    training_features, non_rare_feature_set = remove_rare_features(training_features, 5)
+    for i in range(len(Y_pred)):
+        for k in range(len(tagset)):
+            for j in range(len(tagset)):
+                T1[j] = V[i, j] + Y_pred[i, j, k]
+            V[i + 1, k] = numpy.amax(T1)
+            BP[i + 1, k] = numpy.argmax(T1)
 
-    with open("updated_feature.txt", "w+") as f:
-        f.write(str(training_features))
+    backward_indices = []
+    index = numpy.argmax(V[n - 1])
+    backward_indices.append(index)
+    while n != 1:
+        index = BP[n - 1, index]
+        backward_indices.append(index)
+        n = n - 1
+    backward_indices.reverse()
+    inv_map = {v: k for k, v in tag_dict.items()}
+    result = []
+    for i in backward_indices:
+        result.append(inv_map[i])
+    return result
 
-    with open("non_rare_feature.txt", "w+") as f:
-        f.write(str(non_rare_feature_set))
-
-    # Creating feature dict from non-rare feature set.
-
-    feature_dict = dict()
-    tag_dict = dict()
-
-    index = 0
-    for feature in non_rare_feature_set:
-        feature_dict[feature] = index
-        index = index + 1
-
-    tag_set = set()
-
-    for sen_tags in train_tags:
-        for tags in sen_tags:
-            tag_set.add(tags)
-
-    print(set(tag_set))
-    index = 0
-    for tag in tag_set:
-        tag_dict[tag] = index
-        index = index + 1
-
-    build_Y(train_tags, tag_dict)
 
 def remove_rare_features(features, n):
     feature_count = dict()
@@ -195,6 +187,84 @@ def remove_rare_features(features, n):
     # print("Updated feature: " + str(updated_features))
     # print("Non rare: " + str(non_rare_feature_set))
     return updated_features, non_rare_feature_set
+
+
+def main():
+    global tagset, tag_dict, feature_dict, rare_words 
+    brown_sentences = brown.tagged_sents(tagset='universal')
+    print("Loaded the corpus.")
+
+    # Initializing training sentences and training tags.
+
+    train_sentences = list(list())
+    train_tags = list(list())
+    words_count = dict()
+    rare_words = set()
+    for sen_tag_list in brown_sentences:
+        temp_word = list()
+        temp_tag = list()
+        for word, tag in sen_tag_list:
+            temp_word.append(word)
+            temp_tag.append(tag)
+            count = words_count.get(word, 0)
+            words_count[word] = count + 1
+        train_sentences.append(temp_word)
+        train_tags.append(temp_tag)
+    for k, v in words_count.items():
+        if v < 5:
+            rare_words.add(k)
+
+    # Generating features for all training sentences.
+
+    training_features = list(list())
+    for i in range(len(train_sentences)):
+        temp_sen_features = list()
+        for j in range(len(train_sentences[i])):
+            temp_word_features = list()
+            if j == 0:
+                temp_word_features = temp_word_features + get_features(j, train_sentences[i], "<S>", rare_words)
+            else:
+                temp_word_features = temp_word_features + get_features(j, train_sentences[i], train_tags[i][j - 1],
+                                                                       rare_words)
+            temp_sen_features.append(temp_word_features)
+        training_features.append(temp_sen_features)
+
+
+    training_features, non_rare_feature_set = remove_rare_features(training_features, 5)
+
+    # Creating feature dict from non-rare feature set.
+
+    index = 0
+    for feature in non_rare_feature_set:
+        feature_dict[feature] = index
+        index = index + 1
+
+    tag_set = set()
+    for sen_tags in train_tags:
+        for tags in sen_tags:
+            tag_set.add(tags)
+
+    tagset = list(tag_set)
+
+    index = 0
+    for tag in tag_set:
+        tag_dict[tag] = index
+        index = index + 1
+
+    # Training the model...
+    print("Training the model.")
+    X_train = build_X(training_features)
+    Y_train = build_Y(train_tags)
+    obj.fit(X_train, Y_train)
+
+    # Testing the model...
+    print("Testing the model.")
+    test_data = load_test("test.txt")
+    for line in test_data:
+        if line is not None:
+            Y, X = get_predictions(line, obj)
+            res = viterbi(X, Y)
+            print(res)
 
 
 if __name__ == '__main__':
